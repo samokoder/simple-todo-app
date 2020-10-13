1f6750ce1fb0bb622c7ad0ca677186ca2bd7f233
samokoder <samokoder@hashboard.ru>
2020-10-13 17:02:36 +0300

    UPD: db-scheme

---
+++ db/migrate/20201013135155_add_todos_table.rb
@@ -0,0 +1,9 @@
+class AddTodosTable < ActiveRecord::Migration[6.0]
+  def change
+    create_table :todos do |t|
+      t.string :title, null: false
+      t.boolean :done, default: false
+      t.timestamps
+    end
+  end
+end
